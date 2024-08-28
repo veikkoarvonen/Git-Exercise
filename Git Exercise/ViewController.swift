b0822aee26647f1425bac5ec80ac6b4550bdb4d6
Veikko Arvonen <arvonenveikko@gmail.com>
2024-08-28 21:08:20 +0300

    Initial Commit

---
+++ Git Exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git Exercise
+//
+//  Created by Veikko Arvonen on 28.8.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
